48e80341be29c760cd4448650efe1ac3feb23aa5
Matheus Alecksander <projetos.matheusalecksander@gmail.com>
2022-05-02 16:07:20 -0300

    Created widget component

--- src/components/Widget/index.tsx
+++ src/components/Widget/index.tsx
@@ -1,5 +1,17 @@
+import { ChatTeardropDots } from 'phosphor-react'
+import { Popover } from '@headlessui/react'
+
 export function Widget() {
+
   return (
-    <h1>Widget</h1>
+    <Popover className='absolute bottom-5 right-5'>
+      <Popover.Panel>Hello World</Popover.Panel>
+
+      <Popover.Button className='flex items-center rounded-full bg-brand-500 px-3 h-12 text-white group'>
+        <ChatTeardropDots className='w-6 h-6' />
+        <span className='max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear'>Feedback</span>
+      </Popover.Button>      
+    </Popover>
+
   )
 }
